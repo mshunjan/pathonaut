dc537b3663d86e4b284138327d5707658f143291
mshunjan <manjothunjan@gmail.com>
2025-04-24 15:54:40 -0400

    feat: seperated homeview into modular section

---
+++ src/sections/home/view.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Logo from "@/components/logo";
+import ThemeToggle from "@/components/theme-toggle"; 
+import * as React from "react"; 
+import FileSelectForm from "./file-select-form";
+
+export default function Home() {
+  
+  return (
+    <main className="relative flex min-h-screen flex-col">
+
+
+      <header className="w-full flex items-center justify-between p-4">
+        <Logo full={false} />
+        <ThemeToggle />
+      </header>
+      <div className="flex-1 flex items-center justify-center">
+        <FileSelectForm />
+      </div>
+    </main>
+  );
+}
